6959be7f999d8606033785e0e4bf2320a613b334
Sborshikow Pavel <sborshikow@gmail.com>
2024-10-21 14:01:57 +0700

    [1.2.1] Improved TypeSafeEnum

--- Runtime/Utilities/TypeSafeEnum.cs
+++ Runtime/Utilities/TypeSafeEnum.cs
@@ -35,18 +35,70 @@ namespace ED.Additional.Utilities
         public static bool operator !=(TypeSafeEnum left, TypeSafeEnum right) => !(left == right);
     }
 
-    public abstract class TypeSafeEnum<T> : TypeSafeEnum where T : TypeSafeEnum<T>
+    public abstract class TypeSafeEnum<TSelf> : TypeSafeEnum where TSelf : TypeSafeEnum<TSelf>
     {
-        public static readonly IReadOnlyList<T> Values;
+        public static readonly IReadOnlyList<TSelf> Values;
 
         static TypeSafeEnum()
         {
-            Values = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Static)
-                .Where(a => a.PropertyType == typeof(T))
-                .Select(a => (T)a.GetValue(null))
+            Values = typeof(TSelf).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Where(a => a.PropertyType == typeof(TSelf))
+                .Select(a => (TSelf)a.GetValue(null))
                 .ToList();
         }
 
         protected TypeSafeEnum(string name) : base(name) { }
+    }
+
+    public abstract class TypeSafeValueEnum<TValue>
+    {
+        private readonly string _name;
+        private readonly TValue _value;
+
+        protected TypeSafeValueEnum(string name, TValue value)
+        {
+            _name = name ?? string.Empty;
+            _value = value;
+        }
+
+        public override string ToString() => _name;
+
+        public static implicit operator string(TypeSafeValueEnum<TValue> value) => value._name;
+        public static implicit operator TValue(TypeSafeValueEnum<TValue> value) => value._value;
+
+        private bool Equals(TypeSafeValueEnum<TValue> other) => _value.Equals(other._value);
+
+        public override bool Equals(object other)
+        {
+            if (ReferenceEquals(other, null)) return false;
+            if (ReferenceEquals(other, this)) return true;
+            if (other.GetType() != GetType()) return false;
+            return Equals((TypeSafeValueEnum<TValue>)other);
+        }
+
+        public override int GetHashCode() => _value.GetHashCode();
+
+        public static bool operator ==(TypeSafeValueEnum<TValue> left, TypeSafeValueEnum<TValue> right)
+        {
+            if (ReferenceEquals(left, null)) return ReferenceEquals(right, null);
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(TypeSafeValueEnum<TValue> left, TypeSafeValueEnum<TValue> right) => !(left == right);
+    }
+
+    public abstract class TypeSafeValueEnum<TValue, TSelf> : TypeSafeValueEnum<TValue> where TSelf : TypeSafeValueEnum<TValue, TSelf>
+    {
+        public static readonly IReadOnlyList<TSelf> Values;
+
+        static TypeSafeValueEnum()
+        {
+            Values = typeof(TSelf).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Where(a => a.PropertyType == typeof(TSelf))
+                .Select(a => (TSelf)a.GetValue(null))
+                .ToList();
+        }
+
+        protected TypeSafeValueEnum(string name, TValue value) : base(name, value) { }
     }
 }
